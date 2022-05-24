c9c8e549867a201ebd5d43e8f46dc0c15e210a7f
Dawit <dawityab@gmail.com>
2022-05-24 09:59:41 +0200

    inital commit

---
+++ main.js
@@ -0,0 +1,63 @@
+// let goodSkils = [
+//     {
+//         name:"HTML5",
+//         image:"img/html.png"
+//     },
+//     {
+//         name:"CSS3",
+//         image:"img/css.png"
+//     },
+//     {
+//         name:"JS",
+//         image:"img/js.png"
+//     },
+//     {
+//         name:"Ts",
+//         image:"img/ts.png"
+//     },
+//     {
+//         name:"Bootstrap",
+//         image:"img/bootstrap.png"
+//     },
+//     {
+//         name:"Sass",
+//         image:"img/sass.png"
+//     },
+//     {
+//         name:"Git",
+//         image:"img/git.jpg"
+//     },
+//     {
+//         name:"PHP",
+//         image:"img/php.jpg"
+//     },
+   
+  
+// ];
+// let basicSkils = [
+//     {
+//         name:"React Js",
+//         image:"img/react.png"
+//     },
+//     {
+//         name:"Node",
+//         image:"img/node.png"
+//     }
+// ];
+// for(let i in goodSkils){
+
+
+// document.getElementById("result").innerHTML += `
+
+// <div class="col-lg-4 col-md-6 col-sm-12">
+//     <div class="card m-1 mx-3 pb-2 cards-style ">
+   
+//     <img src="${goodSkils[i].image}" class="card-img-top" alt="${goodSkils[i].name}"> 
+    
+//       <h3 class="card-text text-light bg-dark text-center fs-5 p-2">${goodSkils[i].name}</h3>
+    
+// </div>
+
+// </div>
+// `;
+// }
